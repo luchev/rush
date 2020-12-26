8d9a475fa0a1ec524c6d47aa51391dbc691f4c3b
luchev <luchevz@gmail.com>
2020-12-26 20:34:54 +0200

    Add libc binding for finding home dir

---
+++ src/libc_bindings/mod.rs
@@ -0,0 +1,24 @@
+use libc;
+use std::ffi::{CString, CStr};
+
+pub fn user_home_dir_by_user_name(name: &str) -> Result<String, String> {
+    let user = match CString::new(name) {
+        Ok(x) => x,
+        Err(_) => return Err("Failed to convert username to c-string".to_string()),
+    };
+
+    unsafe {
+        let passwd_ptr = libc::getpwnam(user.as_ptr());
+        let passwd = *passwd_ptr;
+        let name = match CStr::from_ptr(passwd.pw_dir).to_str() {
+            Ok(x) => x.to_string(),
+            Err(_) => {
+                libc::free(passwd_ptr as *mut libc::c_void);
+                return Err("Failed to convert user home directory to UTF8".to_string());
+            }
+        };
+        
+        // libc::free(passwd_ptr as *mut libc::c_void);
+        return Ok(name);
+    };
+}
