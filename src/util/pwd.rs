68d1eb63bf697c3da0e2ebe46fb43ce7a0f5952a
luchev <luchevz@gmail.com>
2020-12-26 20:38:58 +0200

    Refactor OsString to String

--- src/util/pwd.rs
+++ src/util/pwd.rs
@@ -1,14 +1,13 @@
 use std::env;
-use std::ffi::OsString;
 
 /// Return directory portion of pathname
-fn pwd(paths: &[&str]) -> Vec<Result<OsString, OsString>> {
-    if paths.len() > 0 {
-        vec![Err(OsString::from("Too many arguments"))]
+pub fn pwd(args: &[&str]) -> Vec<Result<String, String>> {
+    if args.len() > 0 {
+        vec![Err(String::from("Too many arguments\n"))]
     } else {
         match env::current_dir() {
-            Ok(x) => vec![Ok(x.into_os_string())],
-            Err(x) => vec![Err(x.to_string().into())],
+            Ok(x) => vec![Ok(format!("{}\n", x.into_os_string().to_str().unwrap()))],
+            Err(x) => vec![Err(format!("{}\n", x))],
         }
     }
 }
@@ -21,13 +20,13 @@ mod tests {
     #[test]
     fn test_pwd() {
         let _ = env::set_current_dir("/");
-        assert_eq!("/", pwd(&[])[0].as_ref().unwrap());
+        assert_eq!("/", pwd(&[])[0].as_ref().unwrap().trim());
 
         let _ = env::set_current_dir("/etc");
-        assert_eq!("/etc", pwd(&[])[0].as_ref().unwrap());
+        assert_eq!("/etc", pwd(&[])[0].as_ref().unwrap().trim());
 
         let _ = env::set_current_dir("/usr");
-        assert_eq!("/usr", pwd(&[])[0].as_ref().unwrap());
+        assert_eq!("/usr", pwd(&[])[0].as_ref().unwrap().trim());
         
         assert!(pwd(&["home"])[0].is_err());
     }
