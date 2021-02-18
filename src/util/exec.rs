f1d14044ab2d055d949ddfeeabd359817f620799
luchev <luchevz@gmail.com>
2021-02-18 23:54:24 +0200

    Feat add exec builtin utility

---
+++ src/util/exec.rs
@@ -0,0 +1,24 @@
+use std::os::unix::process::{ExitStatusExt, CommandExt};
+use std::process::ExitStatus;
+
+/// Exit with an exit code
+pub fn exec(args: &[&str]) -> ExitStatus {
+    if args.is_empty() {
+        return ExitStatusExt::from_raw(1);
+    }
+
+    let err = std::process::Command::new(args[0]).args(&args[1..]).exec();
+    eprintln!("Exec error: {}", err);
+    std::process::exit(1);
+}
+
+#[cfg(test)]
+mod tests {
+    use pretty_assertions::{assert_eq};
+    use super::*;
+
+    #[test]
+    fn test_exec_err() {
+        assert_eq!(exec(&[]), ExitStatusExt::from_raw(1));
+    }
+}
