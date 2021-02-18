1cbea2168fad7cc3c1a6815173c238bc4516355d
luchev <luchevz@gmail.com>
2021-02-18 23:54:42 +0200

    Feat add exit builtin utility

---
+++ src/util/exit.rs
@@ -0,0 +1,34 @@
+use std::os::unix::process::ExitStatusExt;
+use std::process::ExitStatus;
+
+/// Exit with an exit code
+pub fn exit(args: &[&str]) -> ExitStatus {
+    if args.len() >= 2 {
+        eprintln!("Too many arguments");
+        return ExitStatusExt::from_raw(1);
+    }
+
+    if args.len() == 1 {
+        match args[0].parse::<i32>() {
+            Ok(exit_code) => std::process::exit(exit_code),
+            Err(_) => {
+                eprintln!("Expected argument to be an integer");
+                return ExitStatusExt::from_raw(2);
+            }
+        }
+    }
+
+    std::process::exit(0);
+}
+
+#[cfg(test)]
+mod tests {
+    use pretty_assertions::{assert_eq};
+    use super::*;
+
+    #[test]
+    fn test_exit_err() {
+        assert_eq!(exit(&["123", "123"]), ExitStatusExt::from_raw(1));
+        assert_eq!(exit(&["wrong args"]), ExitStatusExt::from_raw(2));
+    }
+}
